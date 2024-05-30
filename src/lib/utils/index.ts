3b6cd0d6716586f343b10a70f5816143374c428f
StarPandaBeg <kshmurakov@mail.ru>
2024-05-30 14:27:27 +0300

    feat: add random color helper

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+export * from "./color";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
